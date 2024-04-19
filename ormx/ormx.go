c12d86ee802aa1e9c1de08e0ae3158fdaa12ebe1
hugo2lee <hugo2lee@gmail.com>
2024-04-19 16:56:30 +0800

    feat:ormx push

---
+++ ormx/ormx.go
@@ -0,0 +1,89 @@
+/*
+ * @Author: hugo
+ * @Date: 2024-04-19 16:18
+ * @LastEditors: hugo
+ * @LastEditTime: 2024-04-19 16:24
+ * @FilePath: \gotox\ormx\ormx.go
+ * @Description:
+ *
+ * Copyright (c) 2024 by hugo, All Rights Reserved.
+ */
+package ormx
+
+import (
+	"context"
+	"sync"
+
+	"github.com/hugo2lee/gotox/configx"
+	"github.com/hugo2lee/gotox/logx"
+	"github.com/pkg/errors"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
+
+type Orm struct {
+	gorm   *gorm.DB
+	logger logx.Logger
+}
+
+func New(conf *configx.ConfigCli, logCli logx.Logger) (*Orm, error) {
+	dsn := conf.MysqlDsn()
+	if dsn == "" {
+		return nil, errors.New("mysql dsn is empty")
+	}
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		// 使用 DEBUG 来打印
+		// Logger: glogger.New(gormLoggerFunc(logCli.Debug),
+		// 	glogger.Config{
+		// 		SlowThreshold: 1 * time.Millisecond,
+		// 		LogLevel:      glogger.Info,
+		// 	}),
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true, // 使用单数表名
+		},
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "mysql connect error")
+	}
+
+	switch conf.Mode() {
+	case configx.RUNDEV:
+		db = db.Debug()
+	case configx.RUNTEST:
+		db = db.Debug()
+	default:
+		db = db.Debug()
+	}
+
+	return &Orm{db, logCli}, nil
+}
+
+func (c *Orm) DB() *gorm.DB {
+	return c.gorm
+}
+
+func (c *Orm) Name() string {
+	return "orm"
+}
+
+func (c *Orm) Close(ctx context.Context, wg *sync.WaitGroup) {
+	db, err := c.gorm.DB()
+	if err != nil {
+		c.logger.Error("gorm DB get %v", err)
+		return
+	}
+	if err := db.Close(); err != nil {
+		c.logger.Error("gorm close %v", err)
+		return
+	}
+	wg.Done()
+	c.logger.Info("%s close", c.Name())
+}
+
+type gormLoggerFunc func(msg string, args ...any)
+
+func (g gormLoggerFunc) Printf(msg string, args ...any) {
+	g(msg, args)
+}
---
+++ ormx/ormx_test.go
@@ -0,0 +1,39 @@
+/*
+ * @Author: hugo
+ * @Date: 2024-04-19 16:24
+ * @LastEditors: hugo
+ * @LastEditTime: 2024-04-19 16:46
+ * @FilePath: \gotox\ormx\ormx_test.go
+ * @Description:
+ *
+ * Copyright (c) 2024 by hugo, All Rights Reserved.
+ */
+package ormx_test
+
+import (
+	"testing"
+
+	"github.com/hugo2lee/gotox/configx"
+	"github.com/hugo2lee/gotox/logx"
+	"github.com/hugo2lee/gotox/ormx"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMysql(t *testing.T) {
+	t.Parallel()
+	conf := configx.New(configx.WithPath("../conf"))
+	logCli := logx.New(conf)
+	dbGorm, err := ormx.New(conf, logCli)
+	db := dbGorm.DB()
+	assert.NoError(t, err)
+	assert.NotNil(t, db)
+
+	type User struct {
+		Name string
+	}
+	err = db.AutoMigrate(&User{})
+	assert.NoError(t, err)
+
+	err = db.Model(&User{}).Create(&User{Name: "hugo"}).Error
+	assert.NoError(t, err)
+}
