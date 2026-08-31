8489120212c4eb89834cbf14a3edd2d8e3fc93be
hugo2lee <hugo2lee@gmail.com>
2026-08-31 16:57:09 +0800

    test: cover mongo database wiring seam

--- mongox/mongox_test.go
+++ mongox/mongox_test.go
@@ -19,7 +19,19 @@ import (
 	"github.com/hugo2lee/gotox/logx"
 	"github.com/hugo2lee/gotox/mongox"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+func TestMongoDatabaseWiring(t *testing.T) {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
+	require.NoError(t, err)
+	database := client.Database("test")
+
+	wrapped := mongox.NewWithDatabase(database, nil)
+	assert.Same(t, database, wrapped.DB())
+}
 
 func TestMongo(t *testing.T) {
 	t.Parallel()
