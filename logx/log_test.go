b99bcb19fdabd0cf309b8878dd05710fa15ad3e3
hugo2lee <hugo2lee@gmail.com>
2026-08-31 17:04:59 +0800

    test: make logger test self contained

--- logx/log_test.go
+++ logx/log_test.go
@@ -11,23 +11,27 @@
 package logx_test
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/hugo2lee/gotox/configx"
 	"github.com/hugo2lee/gotox/logx"
+	"github.com/stretchr/testify/require"
 )
 
 func TestLogger(t *testing.T) {
-	conf := configx.New(configx.WithPath("../conf"))
-	logger := logx.New(conf)
+	dir := t.TempDir()
+	logFile := filepath.Join(dir, "logs", "test.log")
+	configContent := fmt.Sprintf("[log]\ndir = %q\n", filepath.ToSlash(logFile))
+	require.NoError(t, os.WriteFile(filepath.Join(dir, "dev.toml"), []byte(configContent), 0o600))
 
-	// build random log for check max size
-	for i := 0; i < 10; i++ {
-		logger.Debug("debug %v", i)
-		logger.Info("info %v", i)
-	}
-	// logger.Debug("debug %v", "debug")
-	// logger.Info("info %v", "info")
-	// logger.Warn("warn %v", "warn")
-	// logger.Error("error %v", errors.New("test"))
+	conf := configx.New(configx.WithPath(dir), configx.WithMode(configx.RUNDEV))
+	logger := logx.New(conf)
+	logger.Debug("debug %v", 1)
+	logger.Info("info %v", 1)
+
+	_, err := os.Stat(logFile)
+	require.NoError(t, err)
 }
