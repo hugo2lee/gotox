e40fa39bc321cea89442358071a946116a9704a1
hugo2lee <hugo2lee@gmail.com>
2026-08-31 16:16:46 +0800

    refactor: inject hash response logger via builder

--- webx/middleware/hashresponse/hashresponse.go
+++ webx/middleware/hashresponse/hashresponse.go
@@ -15,15 +15,6 @@ import (
 	"github.com/hugo2lee/gotox/logx"
 )
 
-// 受制于泛型，这里只能使用包变量，如无任何实例赋予就用这个
-var logg logx.Logger = logx.Log
-
-// // 自定义的logger，建议实例化赋予
-func SetLogger(l logx.Logger) {
-	logg = l
-}
-
-// HashAlgorithm 定义支持的哈希算法类型
 type HashAlgorithm string
 
 const (
@@ -32,55 +23,60 @@ const (
 	SHA256 HashAlgorithm = "Sha256"
 )
 
-// HashBuilder 是中间件构建器
 type HashBuilder struct {
 	algorithms map[HashAlgorithm]func() hash.Hash
+	logger     logx.Logger
 }
 
-// NewBuilder 创建一个新的构建器实例
 func NewBuilder() *HashBuilder {
 	return &HashBuilder{
 		algorithms: make(map[HashAlgorithm]func() hash.Hash),
+		logger:     logx.NewNoOpLogger(),
 	}
 }
 
-func (hashBuilder *HashBuilder) WithMd5() *HashBuilder {
-	hashBuilder.algorithms[MD5] = md5.New
-	return hashBuilder
+func (b *HashBuilder) WithLogger(logger logx.Logger) *HashBuilder {
+	if logger != nil {
+		b.logger = logger
+	}
+	return b
 }
 
-func (hashBuilder *HashBuilder) WithSha1() *HashBuilder {
-	hashBuilder.algorithms[SHA1] = sha1.New
-	return hashBuilder
+func (b *HashBuilder) WithMd5() *HashBuilder {
+	b.algorithms[MD5] = md5.New
+	return b
 }
 
-func (hashBuilder *HashBuilder) WithSha256() *HashBuilder {
-	hashBuilder.algorithms[SHA256] = sha256.New
-	return hashBuilder
+func (b *HashBuilder) WithSha1() *HashBuilder {
+	b.algorithms[SHA1] = sha1.New
+	return b
 }
 
-// WithAlgorithm 添加需要计算的哈希算法
-func (hashBuilder *HashBuilder) WithAlgorithm(algorithm HashAlgorithm, hasherFunc func() hash.Hash) *HashBuilder {
-	hashBuilder.algorithms[algorithm] = hasherFunc
-	return hashBuilder
+func (b *HashBuilder) WithSha256() *HashBuilder {
+	b.algorithms[SHA256] = sha256.New
+	return b
 }
 
-func (hashBuilder *HashBuilder) SetHash(c *gin.Context, hv *bodyTemp) {
+func (b *HashBuilder) WithAlgorithm(algorithm HashAlgorithm, hasherFunc func() hash.Hash) *HashBuilder {
+	b.algorithms[algorithm] = hasherFunc
+	return b
+}
+
+func (b *HashBuilder) SetHash(c *gin.Context, hv *bodyTemp) {
 	if c.Writer.Status() != http.StatusOK {
 		return
 	}
 
-	// 获取响应体内容
 	bodyStr := hv.body.String()
 	if bodyStr == "" {
-		logg.Error("HashResponse bodyStr is empty")
+		b.logger.Error("HashResponse bodyStr is empty")
 		return
 	}
-	for algorithm, hasherFunc := range hashBuilder.algorithms {
+	for algorithm, hasherFunc := range b.algorithms {
 		hasher := hasherFunc()
 		_, err := io.WriteString(hasher, bodyStr)
 		if err != nil {
-			logg.Error("HashResponse WriteString %s %v", algorithm, err)
+			b.logger.Error("HashResponse WriteString %s %v", algorithm, err)
 			continue
 		}
 		hash := hex.EncodeToString(hasher.Sum(nil))
@@ -88,31 +84,22 @@ func (hashBuilder *HashBuilder) SetHash(c *gin.Context, hv *bodyTemp) {
 	}
 }
 
-// Build 构建 Gin 中间件
 func (b *HashBuilder) Build() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// 保存原始 Writer
 		originalWriter := c.Writer
-		// 捕获响应 body
-		hv := &bodyTemp{
-			body: bytes.NewBuffer(nil),
-		}
-		c.Writer = &hashBodyWriter{
-			ResponseWriter: originalWriter,
-			bt:             hv,
-		}
+		hv := &bodyTemp{body: bytes.NewBuffer(nil)}
+		c.Writer = &hashBodyWriter{ResponseWriter: originalWriter, bt: hv}
 		c.Next()
-		// 在请求处理结束后计算哈希值
 		b.SetHash(c, hv)
 		originalWriter.WriteHeaderNow()
 		if hv.write {
 			if _, err := originalWriter.Write(hv.body.Bytes()); err != nil {
-				logg.Error("HashResponse Write %v", err)
+				b.logger.Error("HashResponse Write %v", err)
 			}
 		}
 		if hv.writeString {
 			if _, err := originalWriter.WriteString(hv.body.String()); err != nil {
-				logg.Error("HashResponse WriteString %v", err)
+				b.logger.Error("HashResponse WriteString %v", err)
 			}
 		}
 	}
@@ -124,7 +111,6 @@ type bodyTemp struct {
 	writeString bool
 }
 
-// hashBodyWriter 是一个自定义的 ResponseWriter，用于捕获响应 body
 type hashBodyWriter struct {
 	gin.ResponseWriter
 	bt *bodyTemp
