84a114cf6fabf8eb8ecec52688bf98246b65dbdd
hugo2lee <hugo2lee@gmail.com>
2026-08-31 17:06:01 +0800

    test: mark mongo service test as integration

--- mongox/mongox_test.go
+++ mongox/mongox_test.go
@@ -1,3 +1,5 @@
+//go:build integration
+
 /*
  * @Author: hugo
  * @Date: 2024-04-19 17:23
@@ -19,29 +21,17 @@ import (
 	"github.com/hugo2lee/gotox/logx"
 	"github.com/hugo2lee/gotox/mongox"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func TestMongoDatabaseWiring(t *testing.T) {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
-	require.NoError(t, err)
-	database := client.Database("test")
-
-	wrapped := mongox.NewWithDatabase(database, nil)
-	assert.Same(t, database, wrapped.DB())
-}
-
-func TestMongo(t *testing.T) {
+func TestMongoIntegration(t *testing.T) {
 	t.Parallel()
 	conf := configx.New(configx.WithPath("../conf"))
 	logger := logx.New(conf)
-	db, err := mongox.New(conf, logger)
+	db, err := mongox.Dial(context.Background(), conf, logger)
 	assert.NoError(t, err)
 	assert.NotNil(t, db)
 
-	reslut, err := db.DB().Collection("user").InsertOne(context.TODO(), map[string]string{"name": "hugo"})
+	result, err := db.DB().Collection("user").InsertOne(context.Background(), map[string]string{"name": "hugo"})
 	assert.NoError(t, err)
-	log.Println(reslut)
+	log.Println(result)
 }
